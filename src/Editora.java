7eee8a3f0e11c94a27f4d255c30bb6d0e36de1dd
caio-fabricio <caioealguem@gmail.com>
2023-04-04 08:58:19 -0300

    Cosntrutor de editoras

--- src/Editora.java
+++ src/Editora.java
@@ -3,4 +3,11 @@ public class Editora {
     String nome;
     String email;
 
+    public Editora() {
+        nome = "Editora Horacio"; //ou This.nome = ........
+    }
+
+    public Editora(String nome) {
+        this.nome = "Editora Erica";
+    }
 }
